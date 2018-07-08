545b3ac6e30de60c9cd758cc494c6753d809f91b
john ornelas <johnnyoh1988@gmail.com>
2018-07-08 14:42:10 -0700

    change directory path for GamesList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import GamesList from './src/components/GamesList';
+import GamesList from './src/components/GamesList/GamesList';
 
 export default class App extends React.Component {
     render() {

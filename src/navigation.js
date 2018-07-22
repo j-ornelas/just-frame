0f97bdf4340ebc9ff587284f82fbc93fb4ccf5ae
john ornelas <johnnyoh1988@gmail.com>
2018-07-21 18:25:08 -0700

    Init navigation

---
+++ src/navigation.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+import { TouchableOpacity } from 'react-native';
+import { Icon } from 'native-base';
+import { GamesList, CharacterSelect } from './components/GameStack';
+
+const GameStack = createStackNavigator({
+    GamesList: {
+        screen: GamesList,
+        navigationOptions: ({ navigation }) => ({
+            title: 'Games',
+            drawerLabel: 'GamesList',
+            // headerRight: (
+            //     <TouchableOpacity onPress={() => navigation.navigate('GamesList')}>
+            //         <Icon
+            //             style={{ paddingRight: 5 }}
+            //             name="menu"
+            //         />
+            //     </TouchableOpacity>
+            // )
+        })
+    },
+    CharacterSelect: {
+        screen: CharacterSelect,
+        navigationOptions: ({ navigation }) => ({
+            title: 'Character Select',
+            // headerRight: (
+            //     <TouchableOpacity onPress={() => navigation.navigate('GamesList')}>
+            //         <Icon
+            //             style={{ paddingRight: 5 }}
+            //             name="menu"
+            //             navigation={navigation}
+            //         />
+            //     </TouchableOpacity>
+            // )
+        })
+    },
+}, {
+    initialRouteName: 'GamesList',
+});
+
+const RootStack = createStackNavigator({
+    // TODO: add login, calculator stacks
+    main: { screen: GameStack }
+}, {
+    headerMode: 'none',
+    initialRouteName: 'main'
+});
+
+
+export default RootStack;

a47f7931959c57c5c09dc42da596a79e15af9ba6
john ornelas <johnnyoh1988@gmail.com>
2018-07-21 18:22:46 -0700

    Add logger, change gameslist to rootstack

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
-import reducers from './src/redux/reducers';
 
-import GamesList from './src/components/GamesList/GamesList';
+import reducers from './src/redux/reducers';
+import RootStack from './src/navigation';
+
+const logger = createLogger({
+    // ...options
+});
+
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk, logger));
 
 export default class App extends React.Component {
     render() {
-        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
-
         return (
             <Provider store={store}>
-                <GamesList />
+                <RootStack />
             </Provider>
         );
     }
